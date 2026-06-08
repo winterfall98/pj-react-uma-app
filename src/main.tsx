2c1289ea78a38eff1b18598bbcda11c8b9a51480
Mitch <dontellies93@gmail.com>
2026-06-08 16:07:10 +0900

    폰트 추가, 이미지어셋 추가 등

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './assets/styles/reset.css'
+import './assets/styles/utilities.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
